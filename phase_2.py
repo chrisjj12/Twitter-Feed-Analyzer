8995aad2406567df9cbeb406b3c919e3f529588f
Christopher Jung <chrisjung@MacBook-Pro.fios-router.home>
2020-10-12 00:00:54 -0400

    Phase 1 and 2 complete

---
+++ phase_2.py
@@ -0,0 +1,69 @@
+import keys
+import tweepy
+import time
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+import argparse
+
+
+def choose_hashtag(hashtags):
+    auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+    auth.set_access_token(keys.access_token, keys.access_secret)
+    api = tweepy.API(auth)
+
+    public_tweets = api.search(q = hashtags)
+
+    return public_tweets
+
+def print_result(annotations):
+    score = annotations.document_sentiment.score
+    magnitude = annotations.document_sentiment.magnitude
+
+    for index, sentence in enumerate(annotations.sentences):
+        sentence_sentiment = sentence.sentiment.score
+        print(
+            "Sentence {} has a sentiment score of {}".format(index, sentence_sentiment)
+        )
+
+    print(
+        "Overall Sentiment: score of {} with magnitude of {}".format(score, magnitude)
+    )
+    return 0
+
+def analyze(tweet): 
+    """Run a sentiment analysis request on text within a passed filename."""
+    client = language.LanguageServiceClient()
+
+    document = types.Document(content=tweet, type=enums.Document.Type.PLAIN_TEXT)
+    annotations = client.analyze_sentiment(document=document)
+
+    # Print the results
+    print_result(annotations)
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "--twitter_hashtag",
+        help="Username of desired twitter account.",
+    )
+    parser.add_argument(
+        "--num_tweets",
+        help="Define the number of tweets to analyze.",
+    )
+
+    args = parser.parse_args()
+
+    tweets = choose_hashtag(args.twitter_hashtag)
+
+    for i in range(int(args.num_tweets)):
+        print("Tweet {}".format(i))
+        print(tweets[i].text)
+        analyze(tweets[i].text) 
+    
+    
