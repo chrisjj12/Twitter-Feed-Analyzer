4b872eb6ebb8fe2380befac389de0e40e2eb0715
Christopher Jung <chrisjung@MacBook-Pro.fios-router.home>
2020-10-12 00:26:14 -0400

    REMOVED API KEYS

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
-consumer_key = "jJL5qh2B0TBLfi07tjcykl3gW"
-consumer_secret = "I4l7TTHT4ZhTCQ8iDeiPxXNIfOQ7NaRM3dUszuMx8U90SCggWC"
-access_token = "1224415448571056128-rDIt4nLDw7QDD05FLIionzrOd2Bjbt"
-access_secret = "k4HuxRUTxi4Qep1bjBRqJRLwrzgnbVkU92MqXEr0tGUmA"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_secret = ""
