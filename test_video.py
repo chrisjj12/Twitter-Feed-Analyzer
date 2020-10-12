4132cdfee72f8e09301770fefc9f002d46ed31c7
Christopher Jung <chrisjung@MacBook-Pro.fios-router.home>
2020-10-11 22:02:42 -0400

    google nlp works perfectly, receiving sentiment analysis

--- test_video.py
+++
@@ -1,9 +0,0 @@
-import twitterapi
-
-def test_results():
-    assert twitterapi.choose_username("realmadrid") == 0
-
-
-if __name__ == "__main__":
-    print(test_results())
-
--- twitterapi.py
+++ twitterapi.py
@@ -1,9 +1,6 @@
 import keys
 import tweepy
-#import test_video
 import time
-#import re 
-#from nltk.tokenize import WordPunctTokenizer
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
@@ -15,7 +12,7 @@ def choose_username(username):
     auth.set_access_token(keys.access_token, keys.access_secret)
     api = tweepy.API(auth)
 
-    public_tweets = api.user_timeline(id = "realmadrid")
+    public_tweets = api.user_timeline(id = username)
 
     return public_tweets
 
@@ -34,18 +31,11 @@ def print_result(annotations):
     )
     return 0
 
-
-
-
-def analyze(public_tweets):
+def analyze(tweet): 
     """Run a sentiment analysis request on text within a passed filename."""
     client = language.LanguageServiceClient()
 
-    with open(public_tweets, "r") as review_file:
-        # Instantiates a plain text document.
-        content = review_file.read()
-
-    document = type.Document(content=content, type=enums.Document.Type.PLAIN_TEXT)
+    document = types.Document(content=tweet, type=enums.Document.Type.PLAIN_TEXT) #tweet.?
     annotations = client.analyze_sentiment(document=document)
 
     # Print the results
@@ -59,9 +49,21 @@ if __name__ == "__main__":
         description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
     )
     parser.add_argument(
-        "public_tweets",
-        help="The filename of the movie review you'd like to analyze.",
+        "--twitter_username",
+        help="Username of desired twitter account.",
     )
+    parser.add_argument(
+        "--num_tweets",
+        help="Define the number of tweets to analyze.",
+    )
+
     args = parser.parse_args()
 
-    analyze(args.public_tweets)
+    tweets = choose_username(args.twitter_username)
+
+    for i in range(int(args.num_tweets)):
+        print("Tweet {}".format(i))
+        print(tweets[i].text)
+        analyze(tweets[i].text) 
+    
+    
